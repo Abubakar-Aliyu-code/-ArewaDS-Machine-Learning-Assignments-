787e7f09134fed0430212c3b369f586f10763116
Abubakar-Aliyu-code <abubakaraliyu599@gmail.com>
2023-05-21 11:29:15 +0100

    updated

---
+++ Week 3; Data Visualization/Dangerous Liaisons Data Visualization Project/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Nodes />
+  </div>
+</template>
+
+<script>
+import Nodes from "./assets/Nodes";
+
+export default {
+  name: "App",
+  assets: {
+    Nodes,
+  },
+};
+</script>
